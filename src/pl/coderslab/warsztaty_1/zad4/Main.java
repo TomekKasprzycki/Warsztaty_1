48467c29cb3a4cd02c13d7bc850ddcc34d1f34aa
TomekKasprzycki <tomasz.kasprzycki@gmail.com>
2019-06-15 15:44:57 +0200

    Dodano zadanie z biblioteką jsoup

--- src/pl/coderslab/warsztaty_1/zad4/Main.java
+++ src/pl/coderslab/warsztaty_1/zad4/Main.java
@@ -1,4 +1,32 @@
 package pl.coderslab.warsztaty_1.zad4;
 
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+import java.io.IOException;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Connection connect = Jsoup.connect("http://www.onet.pl/");
+        try {
+            Document document = connect.get();
+            Elements links = document.select("span.title");
+            for (Element elem : links) {
+                System.out.println(elem.text());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
+
+
 }
